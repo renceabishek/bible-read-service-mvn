65d02490f90abd31a87608d7746d10101ab8ab3d
renceabi <renceabishek@gmail.com>
2020-01-20 22:08:23 +0530

    checking

--- src/main/java/com/bible/read/config/FirebaseConfiguration.java
+++ src/main/java/com/bible/read/config/FirebaseConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ExtConfig {
+public class FirebaseConfiguration {
 	
 	@Value("${pi.value}")
 	private String path;
