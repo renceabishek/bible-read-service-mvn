6ef4f07f6e8447925c9471b58de1990639ec9ae1
renceabi <renceabishek@gmail.com>
2020-01-20 21:10:35 +0530

    checking

--- src/main/java/com/bible/bibleservice/config/FirebaseConfig.java
+++ src/main/java/com/bible/bibleservice/config/FirebaseConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
-@Configuration
+//@Configuration
 public class FirebaseConfig {
 
   /*@Value("${pi.value}")
