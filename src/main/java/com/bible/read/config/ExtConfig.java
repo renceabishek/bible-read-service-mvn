0b08fc7c5548977978ff0216c2a28089eddb1ac7
renceabi <renceabishek@gmail.com>
2020-01-20 21:50:00 +0530

    checking

--- src/main/java/com/bible/read/config/ExtConfig.java
+++ src/main/java/com/bible/read/config/ExtConfig.java
@@ -1,8 +1,12 @@
 package com.bible.read.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ExtConfig {
+	
+	@Value("${pi.value}")
+	private String path;
 
 }
