66a65a868aaec06593252d8fcf27de39ed3f4343
renceabi <renceabishek@gmail.com>
2020-01-20 22:11:28 +0530

    checking

--- src/main/java/com/bible/read/config/FirebaseConfiguration.java
+++ src/main/java/com/bible/read/config/FirebaseConfiguration.java
@@ -1,12 +1,48 @@
 package com.bible.read.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class FirebaseConfiguration {
 	
-	@Value("${pi.value}")
-	private String path;
+	@Value("${firebase.path}")
+	  private String path;
+
+	  @Value(value = "classpath:google-services.json")
+	  private Resource gservicesConfig;
+
+	  @Value("${firebase.database-url}")
+	  private String databaseUrl;
+
+	  /*@Bean
+	  public FirebaseApp provideFirebaseOptions() throws IOException {
+	    FirebaseOptions options = new FirebaseOptions.Builder()
+	        .setCredentials(GoogleCredentials.fromStream((gservicesConfig.getInputStream())))
+	        .setDatabaseUrl(databaseUrl)
+	        .build();
+
+	    return FirebaseApp.initializeApp(options);
+	  }
+
+	  @Bean
+	  @Qualifier("main")
+	  public DatabaseReference provideDatabaseReference(FirebaseApp firebaseApp) {
+	    FirebaseDatabase
+	        .getInstance(firebaseApp)
+	        .setPersistenceEnabled(false);
+	    return FirebaseDatabase
+	        .getInstance(firebaseApp)
+	        .getReference();
+	  }*/
 
 }
