c506f42718f3eb84a1eeb2b943d0eb53bdfddfe6
renceabi <renceabishek@gmail.com>
2020-01-20 22:30:55 +0530

    checking

--- src/main/java/com/bible/read/controller/ReadController.java
+++ src/main/java/com/bible/read/controller/ReadController.java
@@ -3,12 +3,16 @@ package com.bible.read.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bible.read.model.DailyData;
+import com.bible.read.model.TotalCount;
 import com.bible.read.service.DailyDataService;
 
 @RequestMapping("/bible")
@@ -26,5 +30,25 @@ public class ReadController {
 	@GetMapping(value = "/all")
 	  public List<DailyData> getDailyData() {
 	    return dailyDataService.getDailyData();
+	  }
+	
+	@GetMapping(value = "/date/{date}")
+	  public List<DailyData> getDailyDataByDate(@PathVariable String date) {
+	    return dailyDataService.getDailyDataByDate(date);
+	  }
+
+	  @DeleteMapping(value = "/date/{uniqueId}")
+	  public void deleteDailyDataById(@PathVariable String uniqueId) {
+	     dailyDataService.deleteDailyDataById(uniqueId);
+	  }
+
+	  @PostMapping(value = "/all", consumes = "application/json")
+	  public void createDailyData(@RequestBody DailyData dailyData) {
+	     dailyDataService.createDailyData(dailyData);
+	  }
+
+	  @GetMapping(value="/totalCounts")
+	  public List<TotalCount> getTotalCountData() {
+	    return dailyDataService.getTotalCountData();
 	  }
 }
